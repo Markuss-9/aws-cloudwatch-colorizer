44112d1f174ffc6fb14a0d58786608ba0446703d
Markuss-9 <player1.markuss@gmail.com>
2023-11-23 23:16:20 +0100

    webpack config: build colorize.js as boundle to use lodash

--- webpack.config.js
+++ webpack.config.js
@@ -3,95 +3,129 @@ const HTMLPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-	entry: {
-		index: "./src/index.tsx",
-	},
-	mode: "production",
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: [
-					{
-						loader: "ts-loader",
-						options: {
-							compilerOptions: { noEmit: false },
-						},
-					},
-				],
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/i,
-				use: ["style-loader", "css-loader"],
-			},
-			{
-				test: /\.svg$/,
-				use: [
-					{
-						loader: require.resolve("@svgr/webpack"),
-						options: {
-							prettier: false,
-							svgo: false,
-							svgoConfig: {
-								plugins: [{ removeViewBox: false }],
+module.exports = [
+	{
+		entry: {
+			index: "./src/index.tsx",
+		},
+		mode: "production",
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: [
+						{
+							loader: "ts-loader",
+							options: {
+								compilerOptions: { noEmit: false },
 							},
-							titleProp: true,
-							ref: true,
 						},
-					},
-					{
-						loader: require.resolve("file-loader"),
-						options: {
-							name: "static/media/[name].[hash].[ext]",
-						},
-					},
-				],
-				issuer: {
-					and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
-				},
-			},
-		],
-	},
-	plugins: [
-		new CopyPlugin({
-			patterns: [
-				{ from: "src/scripts/manifest.json", to: "../manifest.json" },
-				{ from: "src/scripts/colorize.js", to: "../colorize.js" },
-				{
-					from: "src/scripts/backgroundColor.css",
-					to: "../backgroundColor.css",
+					],
+					exclude: /node_modules/,
 				},
 				{
-					from: "img/aws_colorized.png",
-					to: "../img/aws_colorized.png",
+					test: /\.css$/i,
+					use: ["style-loader", "css-loader"],
+				},
+				{
+					test: /\.svg$/,
+					use: [
+						{
+							loader: require.resolve("@svgr/webpack"),
+							options: {
+								prettier: false,
+								svgo: false,
+								svgoConfig: {
+									plugins: [{ removeViewBox: false }],
+								},
+								titleProp: true,
+								ref: true,
+							},
+						},
+						{
+							loader: require.resolve("file-loader"),
+							options: {
+								name: "static/media/[name].[hash].[ext]",
+							},
+						},
+					],
+					issuer: {
+						and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
+					},
 				},
 			],
-		}),
-		...getHtmlPlugins(["index"]),
-	],
-	resolve: {
-		extensions: [".tsx", ".ts", ".js"],
-	},
-	output: {
-		path: path.join(__dirname, "dist/js"),
-		filename: "[name].js",
-	},
-	optimization: {
-		minimize: true,
-		minimizer: [
-			new TerserPlugin({
-				terserOptions: {
-					format: {
-						comments: false,
+		},
+		plugins: [
+			new CopyPlugin({
+				patterns: [
+					{
+						from: "src/scripts/manifest.json",
+						to: "../manifest.json",
 					},
-				},
-				extractComments: false,
+					{
+						from: "src/scripts/backgroundColor.css",
+						to: "../backgroundColor.css",
+					},
+					{
+						from: "img/aws_colorized.png",
+						to: "../img/aws_colorized.png",
+					},
+					{
+						from: "LICENSE",
+						to: "../",
+					},
+				],
 			}),
+			...getHtmlPlugins(["index"]),
 		],
+		resolve: {
+			extensions: [".tsx", ".ts", ".js"],
+			alias: {
+				lodash: "lodash-es",
+			},
+		},
+		output: {
+			path: path.join(__dirname, "dist/js"),
+			filename: "[name].js",
+		},
+		optimization: {
+			minimize: true,
+			minimizer: [
+				new TerserPlugin({
+					terserOptions: {
+						format: {
+							comments: false,
+						},
+					},
+					extractComments: false,
+				}),
+			],
+		},
 	},
-};
+	{
+		entry: {
+			index: "./src/scripts/colorize.js",
+		},
+		mode: "production",
+		output: {
+			path: path.join(__dirname, "dist"),
+			filename: "colorize.js",
+		},
+		optimization: {
+			minimize: true,
+			minimizer: [
+				new TerserPlugin({
+					terserOptions: {
+						format: {
+							comments: false,
+						},
+					},
+					extractComments: false,
+				}),
+			],
+		},
+	},
+];
 
 function getHtmlPlugins(chunks) {
 	return chunks.map(
